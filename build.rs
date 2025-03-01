101c32190260bf6dcd27fc77b8b2c18a2dd4d2d3
Filip Sund <filip.sund@gmail.com>
2025-03-01 21:44:38 +0100

    Fix Windows icon when cross compiling

--- build.rs
+++ build.rs
@@ -1,13 +1,16 @@
-#[cfg(windows)]
-extern crate winres;
+extern crate winresource;
 
-#[cfg(windows)]
 fn main() {
-    // Set the icon for the Windows executable
-    let mut res = winres::WindowsResource::new();
-    res.set_icon("resources/icon.ico");
-    res.compile().unwrap();
+    // Please note: Using #[cfg(target_os = "windows")] in build.rs may not work
+    // as expected because build.rs is executed on the host. This means that
+    // target_os is always equal to host_os when compiling build.rs. E.g. if we
+    // use rustc on Linux and want to cross-compile binaries that run on
+    // Windows, target_os in build.rs is "linux". However, CARGO_CFG_TARGET_OS
+    // should always be defined and contains the actual target operating system,
+    // though it can only be checked at runtime of the build script.
+    if std::env::var("CARGO_CFG_TARGET_OS").unwrap() == "windows" {
+        let mut res = winresource::WindowsResource::new();
+        res.set_icon("resources/icon.ico");
+        res.compile().unwrap();
+    }
 }
-
-#[cfg(unix)]
-fn main() {}
